54d4d38622b6a4435a2874a871dc79d94f8f2bdb
bkubick <bjk2158@columbia.edu>
2023-11-13 21:14:46 -0600

    Update tool cards with better design for now
    I plan to create actual designs (hopefully something that moves)
    that will replace the images. Ultimately, I wanted something that
    was nicer to look at in the meantime while I create a better look.

--- web-app/src/views/ToolMenu.tsx
+++ web-app/src/views/ToolMenu.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
+import CoverLetterGeneratorImg from 'src/static/img/pages/cover_letter_generator.png';
+import StartupIdeaGeneratorImg from 'src/static/img/pages/startup_idea_generator.png';
 import { addCursorFollowerListener } from 'src/utils/followers';
+
+console.log(CoverLetterGeneratorImg);
+
+interface ToolCard {
+    title: string,
+    description: string,
+    link: string,
+    image?: string,
+}
 
 
 interface Props {}
@@ -11,6 +22,30 @@ interface State {}
 
 
 class ToolMenu extends React.Component<Props, State> {
+
+    toolCards: ToolCard[] = [
+        {
+            title: 'Cover Letter Generator',
+            description: `
+                A tool to generate a cover letter for a job application. This tool uses
+                Chatbot technology to ask you questions about yourself and the job you are
+                applying for. The answers to these questions are then used to generate a
+                tailored cover letter with your information and the job information.
+            `,
+            link: '/tools/cover_letter_generator',
+            image: CoverLetterGeneratorImg,
+        },
+        {
+            title: 'Startup Idea Generator',
+            description: `A tool to generate startup ideas. This tool uses Chatbot technology
+                to ask you questions about yourself and your interests, along with details around
+                the industry or startup idea you want to focus on. The answers to these questions
+                are then used to generate a list of startup ideas for you.
+            `,
+            link: '/tools/startup_idea_generator',
+            image: StartupIdeaGeneratorImg,
+        },
+    ];
 
     constructor(props: Props) {
         super(props);
@@ -24,19 +59,27 @@ class ToolMenu extends React.Component<Props, State> {
         addCursorFollowerListener();
     }
 
+    /**
+     * Renders a tool card.
+     * @param toolCard The tool card to render.
+     * @param index The index of the tool card.
+     */
+    renderToolCard(toolCard: ToolCard, index: number) {
+        return (
+            <div key={index} className='px-5 mb-8 w-full flex h-fit'>
+                <Link to={toolCard.link} className='card p-6 text-white w-full'>
+                    <h2 className='text-2xl mb-2'>{toolCard.title}</h2>
+                    <p className='text-sm'>{toolCard.description}</p>
+                    { toolCard.image ? <img src={toolCard.image} alt={toolCard.title} className='w-full mt-4' /> : null}
+                </Link>
+            </div>
+        )
+    }
+
     render() {
         return (
-            <div className='mt-12 w-full'>
-                <div className='px-5 mb-8 w-full'>
-                    <Link to='/tools/cover_letter_generator' className='card text-white w-full'>
-                        Cover Letter Generator
-                    </Link>
-                </div>
-                <div className='px-5'>
-                    <Link to='/tools/startup_idea_generator' className='card text-white'>
-                        Startup Idea Generator
-                    </Link>
-                </div>
+            <div className='my-12 w-full flex'>
+                { this.toolCards.map((toolCard, index) => this.renderToolCard(toolCard, index)) }
             </div>
         )
     }
