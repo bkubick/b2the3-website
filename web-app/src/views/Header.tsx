d6a0369c8fce01ad0d985963bcfe7d4acc6d5660
bkubick <bjk2158@columbia.edu>
2023-11-15 09:02:30 -0600

    Refactor Header to functional component to allow for useEffect hook

--- web-app/src/views/Header.tsx
+++ web-app/src/views/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from "react-router-dom";
 
 import Logo from 'src/static/img/b_to_the_3_logo.svg';
@@ -10,130 +10,15 @@ interface Section {
 }
 
 
-interface WrappedProps {
+interface Props {
     sections: Section[];
     classNames?: string;
 }
 
-interface Props extends WrappedProps{
-    activeSection: Section;
-}
 
-
-interface State {
-    localActiveSection: Section;
-    startingSection: Section;
-    startingActiveCircleXPosition: number;
-    currentActiveCircleXPosition: number;
-}
-
-
-class Header extends React.Component<Props, State> {
-    navLinkSectionIds: Record<string, string> = {};
-
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            startingActiveCircleXPosition: 0,
-            currentActiveCircleXPosition: 0,
-            localActiveSection: this.props.activeSection,
-            startingSection: this.props.activeSection,
-        };
-
-        this.props.sections.forEach((section: Section) => {
-            this.navLinkSectionIds[section.title] = `nav-link-section-${section.title}`;
-        });
-    }
-
-    componentDidMount(): void {
-        this.addResizeWindowListener();
-        
-        const startingActiveCircleXPosition: number = this.getNavbarSectionXPosition(this.props.activeSection);
-
-        this.setState({
-            startingSection: this.props.activeSection,
-            startingActiveCircleXPosition: startingActiveCircleXPosition,
-            currentActiveCircleXPosition: startingActiveCircleXPosition,
-        });
-    }
-
-    addResizeWindowListener(): void {
-        const startingActiveCircleXPosition: number = this.getNavbarSectionXPosition(this.props.activeSection);
-
-        window.addEventListener('resize', () => {
-            this.setState({
-                startingSection: this.props.activeSection,
-                startingActiveCircleXPosition: startingActiveCircleXPosition,
-                currentActiveCircleXPosition: startingActiveCircleXPosition,
-            });
-        });
-    }
-
-    getNavbarSectionXPosition(section: Section): number {
-        const sectionElement: HTMLElement | null = document.getElementById(this.navLinkSectionIds[section.title]);
-
-        if (!sectionElement) {
-            throw Error(`Navbar Section ${section} does not exist.`)
-        }
-        return sectionElement.offsetLeft;
-    }
-
-    calculateNavbarXPosition(): number {
-        return this.state.currentActiveCircleXPosition - this.state.startingActiveCircleXPosition; 
-    }
-
-    navItem(section: Section): React.JSX.Element {
-        if (this.props.activeSection.title === section.title && this.state.localActiveSection.title != this.props.activeSection.title) {
-            this.setState({
-                localActiveSection: section,
-                currentActiveCircleXPosition: this.getNavbarSectionXPosition(section),
-            });
-        }
-
-        const titleClass = 'leading-4 text-xs uppercase font-bold transition ease-in-out duration-150 hover:-translate-y-0.5 hover:scale-105';
-        const circleClass = `h-2 w-2 bg-primary rounded-full inline-block duration-300`;
-        const activeCircleStyle = {
-            transitionDuration: '300ms',
-            transform: `translateX(${this.calculateNavbarXPosition()}px)`,
-            transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
-        }
-
-        return (
-            <div key={section.title} className='mx-1 text-center'>
-                <Link id={ this.navLinkSectionIds[section.title] } to={section.route} className={ titleClass }>
-                    { section.title }
-                </Link>
-                {
-                    section == this.state.startingSection ? (
-                        <div id="active-nav-circle" className="leading-4" style={ activeCircleStyle }>
-                            <span className={ circleClass }></span>
-                        </div>
-                    ) : ''
-                }
-
-            </div>
-        )
-    }
-
-    render() {
-        return (
-            <div className={`py-6 top-0 backdrop-blur z-10 sticky ${ this.props.classNames ? this.props.classNames : '' }`}>
-                <div className="text-white flex">
-                    <Logo className='logo'/>
-                    <div className='mx-5 flex my-2'>
-                        { this.props.sections.map((section: Section) => {
-                            return this.navItem(section);
-                        })}
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
-
-const HeaderWrapper = (props: WrappedProps) => {
+function Header(props: Props) {
+    // Finding Active Section
     const location = useLocation();
-
     const activeSection = props.sections.find((section: Section) => {
         if (location.pathname === '/' && section.route === '/') {
             return true;
@@ -149,9 +34,100 @@ const HeaderWrapper = (props: WrappedProps) => {
         throw Error(`No active section found for route ${location.pathname}`);
     }
 
+    // Generating Navbar Section Ids for positioning
+    const navLinkSectionIds: Record<string, string> = {};
+    props.sections.forEach((section: Section) => {
+        navLinkSectionIds[section.title] = `nav-link-section-${section.title}`;
+    });
+
+    // Setting states
+    const [localActiveSection, setLocalActiveSection] = useState<Section>(activeSection);
+    const [startingSection, setStartingSection] = useState<Section>(activeSection);
+    const [startingActiveCircleXPosition, setStartingActiveCircleXPosition] = useState<number>(0);
+    const [currentActiveCircleXPosition, setCurrentActiveCircleXPosition] = useState<number>(0);
+
+    /**
+     * Gets the x position of the section.
+     * 
+     * @param section the section to get the x position of.
+     * @returns the x position of the section.
+     */
+    const getNavbarSectionXPosition = (section: Section): number => {
+        const sectionElement: HTMLElement | null = document.getElementById(navLinkSectionIds[section.title]);
+
+        if (!sectionElement) {
+            throw Error(`Navbar Section ${section} does not exist.`)
+        }
+        return sectionElement.offsetLeft;
+    }
+
+    /**
+     * Calculates the x position of the circle.
+     * 
+     * @returns the x position of the circle.
+     */
+    const calculateNavbarXPosition = (): number => {
+        return currentActiveCircleXPosition - startingActiveCircleXPosition; 
+    }
+
+    /**
+     * Generates the nav item for the section.
+     * 
+     * @param section the section to generate the nav item for.
+     * @returns the nav item for the section.
+     */
+    const navItem = (section: Section): React.JSX.Element => {
+        if (activeSection.title === section.title && localActiveSection.title != activeSection.title) {
+            setLocalActiveSection(section);
+            setCurrentActiveCircleXPosition(getNavbarSectionXPosition(section));
+        }
+
+        const titleClass = 'leading-4 text-xs uppercase font-bold transition ease-in-out duration-150 hover:-translate-y-0.5 hover:scale-105';
+        const circleClass = `h-2 w-2 bg-primary rounded-full inline-block duration-300`;
+        const activeCircleStyle = {
+            transitionDuration: '300ms',
+            transform: `translateX(${calculateNavbarXPosition()}px)`,
+            transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+        }
+
+        return (
+            <div key={section.title} className='mx-1 text-center'>
+                <Link id={ navLinkSectionIds[section.title] } to={section.route} className={ titleClass }>
+                    { section.title }
+                </Link>
+                {
+                    section == startingSection ? (
+                        <div id="active-nav-circle" className="leading-4" style={ activeCircleStyle }>
+                            <span className={ circleClass }></span>
+                        </div>
+                    ) : ''
+                }
+            </div>
+        )
+    }
+
+    // Setting Up position on page load
+    useEffect(() => {
+        const startingCircleXPosition: number = getNavbarSectionXPosition(activeSection);
+
+        setStartingSection(activeSection);
+        setStartingActiveCircleXPosition(startingCircleXPosition);
+        setCurrentActiveCircleXPosition(startingCircleXPosition);
+    }, [ props ]);
+
+    // Rendering
     return (
-        <Header {...props} activeSection={ activeSection }/>
+        <div className={`py-6 top-0 backdrop-blur z-10 sticky ${ props.classNames ? props.classNames : '' }`}>
+            <div className="text-white flex">
+                <Logo className='logo'/>
+                <div className='mx-5 flex my-2'>
+                    { props.sections.map((section: Section) => {
+                        return navItem(section);
+                    })}
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default HeaderWrapper;
+export default Header;
