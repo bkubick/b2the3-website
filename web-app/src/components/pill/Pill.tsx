4898907e10cd4c26350f7e5f5e78ff7d27fb51c9
bkubick <bjk2158@columbia.edu>
2023-11-15 18:34:30 -0600

    Add color customization to pill component

--- web-app/src/components/pill/Pill.tsx
+++ web-app/src/components/pill/Pill.tsx
@@ -3,15 +3,26 @@ import React from 'react';
 
 interface Props {
     text: string;
+    textColor?: string;
+    borderColor?: string;
 }
 
 
+/**
+ * Render function for the pill component.
+ * 
+ * @param props the props for the pill component.
+ * @returns the pill component.
+ */
 function Pill(props: Props): React.JSX.Element {
-  return (
-    <span className="border-2 rounded-full px-4 py-1 text-xs mx-1 mb-1 text-primary border-primary">
-        {props.text}
-    </span>
-  );
+    const borderColorClass = props.borderColor ? `border-${props.borderColor}` : 'border-primary';
+    const textColorClass = props.textColor ? `text-${props.textColor}` : 'text-primary';
+
+    return (
+        <span className={`border-2 rounded-full px-4 py-1 text-xs mx-1 mb-1 ${borderColorClass} ${textColorClass}`}>
+            {props.text}
+        </span>
+    );
 }
 
 
