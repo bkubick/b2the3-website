e0f66cf3cfdfeb563708a5fadaf44f85fc270624
bkubick <bjk2158@columbia.edu>
2023-11-23 17:20:58 -0600

    Add text to the healthDashboard sections

--- web-app/src/views/health-dashboard/HealthDashboard.tsx
+++ web-app/src/views/health-dashboard/HealthDashboard.tsx
@@ -46,6 +46,16 @@ function HealthDashboard(): React.JSX.Element {
                 <div className='col-span-3'>
                     <h1 className='text-2xl font-bold mb-2'>Stand Data</h1>
                     <FileUploadForm<Stand> onSubmitHandler={ setStandData } fieldHeaderMapping={ StandDataMapping } idPrefix='stand-data' headerRowIndex={ 1 }/>
+                    <p className='mt-3 text-slate-300 text-sm'>
+                        View your stand data from your Apple Watch. Upload a CSV file containing your stand data to view your stand data using the CSV app,
+                        Simple Health Export CSV, which can be downloaded from the App Store. The CSV file must contain the following columns:
+                        startDate, endDate, unit, value.
+                        <br />
+                        <br />
+                        Stand data is the amount of time you have stood up and moved around for at least one minute during the day. The stand data is
+                        collected by the Apple Watch and is displayed in the Activity app on the Apple Watch and iPhone. The stand data is also
+                        displayed in the Health app on the iPhone.
+                    </p>
                 </div>
                 <div className='col-span-5'>
                     <StandChart standData={ standData } />
@@ -56,8 +66,19 @@ function HealthDashboard(): React.JSX.Element {
                     <EnergyBurnedChart data={ energyData } />
                 </div>
                 <div className='col-span-3'>
-                <h1 className='text-2xl font-bold mb-2'>Energy Burned</h1>
+                    <h1 className='text-2xl font-bold mb-2'>Energy Burned</h1>
                     <FileUploadForm<EnergyBurned> onSubmitHandler={ setEnergyData } fieldHeaderMapping={ EnergyBurnedDataMapping } idPrefix='energy-burned-data' headerRowIndex={ 1 }/>
+                    <p className='mt-3 text-slate-300 text-sm'>
+                        View your energy burned data from your Apple Watch. Upload a CSV file containing your energy burned data to view your
+                        energy burned data using the CSV app, Simple Health Export CSV, which can be downloaded from the App Store. The CSV file
+                        must contain the following columns: startDate, endDate, unit, value.
+                        <br />
+                        <br />
+                        There are two types of energy burned data: active and resting. Using the above mentioned app, you can export active and
+                        basal energy burned data. Basal energy burned is similar to resting energy burned. The difference between basal and resting
+                        energy burned is that basal energy burned is the energy burned while you are resting and awake. Resting energy burned is the
+                        energy burned while you are resting and asleep.
+                    </p>
                 </div>
             </div>
         </m.div>
