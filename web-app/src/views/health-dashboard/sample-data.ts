9c7f9309b9e51f436e66dae5e3957857d49c6da5
bkubick <bjk2158@columbia.edu>
2023-11-23 09:49:56 -0600

    Add sampledata for health page

---
+++ web-app/src/views/health-dashboard/sample-data.ts
@@ -0,0 +1,71 @@
+import { type Stand } from 'src/interface/health/stand';
+
+
+const STAND_DATA: Stand[] = [
+    {
+        id: 'stand-data-1',
+        startDatetime: '2023-11-01 00:00:00',
+        endDatetime: '2023-11-01 00:05:00',
+        unit: 'min',
+        value: 3,
+    },
+    {
+        id: 'stand-data-2',
+        startDatetime: '2023-11-01 00:05:00',
+        endDatetime: '2023-11-01 00:10:00',
+        unit: 'min',
+        value: 4,
+    },
+    {
+        id: 'stand-data-3',
+        startDatetime: '2023-11-01 00:10:00',
+        endDatetime: '2023-11-01 00:15:00',
+        unit: 'min',
+        value: 1,
+    },
+    {
+        id: 'stand-data-4',
+        startDatetime: '2023-11-01 00:15:00',
+        endDatetime: '2023-11-01 00:20:00',
+        unit: 'min',
+        value: 4,
+    },
+    {
+        id: 'stand-data-5',
+        startDatetime: '2023-11-01 00:20:00',
+        endDatetime: '2023-11-01 00:25:00',
+        unit: 'min',
+        value: 2,
+    },
+    {
+        id: 'stand-data-6',
+        startDatetime: '2023-11-01 00:25:00',
+        endDatetime: '2023-11-01 00:30:00',
+        unit: 'min',
+        value: 1,
+    },
+    {
+        id: 'stand-data-7',
+        startDatetime: '2023-11-01 00:30:00',
+        endDatetime: '2023-11-01 00:35:00',
+        unit: 'min',
+        value: 5,
+    },
+    {
+        id: 'stand-data-8',
+        startDatetime: '2023-11-01 00:35:00',
+        endDatetime: '2023-11-01 00:40:00',
+        unit: 'min',
+        value: 3,
+    },
+    {
+        id: 'stand-data-9',
+        startDatetime: '2023-11-01 00:40:00',
+        endDatetime: '2023-11-01 00:45:00',
+        unit: 'min',
+        value: 4,
+    },
+];
+
+
+export { STAND_DATA };
