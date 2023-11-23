9d68051a098337177758eeb312239e960c9a06c3
bkubick <bjk2158@columbia.edu>
2023-11-22 19:30:50 -0600

    Add id handling in csv parser

--- web-app/src/utils/files.tsx
+++ web-app/src/utils/files.tsx
@@ -3,15 +3,23 @@
  */
 
 
+// The type of the function that sets the id of the object.
+type IdSetter = (index: number) => string;
+
+
 /**
  * Parse CSV file to an array of objects.
  * 
  * @param text the CSV file as a string.
  * @param fieldHeaderMapping the mapping of the object fields to the CSV header names.
  * @param headerRowIndex the index of the row containing the header names.
+ * @param idSetter the function that sets the id of the object.
  * @returns the CSV file as an array of objects.
  */
-function parseCSV<T>(text: string, fieldHeaderMapping: Record<string, keyof T>, headerRowIndex: number = 0): T[] {
+function parseCSV<T>(text: string,
+                     fieldHeaderMapping: Record<string, keyof T>,
+                     headerRowIndex: number = 0,
+                     idSetter: IdSetter | undefined = undefined): T[] {
     const regexSplit: RegExp = /,(?=(?:(?:[^"]*"){2})*[^"]*$)/;
 
     const allTextLines = text.split(/\r\n|\n/);
@@ -19,11 +27,19 @@ function parseCSV<T>(text: string, fieldHeaderMapping: Record<string, keyof T>, 
     const rows = allTextLines.slice(headerRowIndex + 1, -1);
     const headerMappingKeys = Object.keys(fieldHeaderMapping);
 
-    const array = rows.map(row => {
+    if (!headers.includes('id') && headerMappingKeys.includes('id')) {
+        headers.push('id');
+    }
+
+    const array = rows.map((row: string, rowIndex: number) => {
         const values = row.split(regexSplit);
         const obj: T = headers.reduce((model: any, header: string, index: number) => {
             if (headerMappingKeys.includes(header)) {
-                model[fieldHeaderMapping[header as keyof typeof fieldHeaderMapping]] = values[index];
+                if (header === 'id' && idSetter) {
+                    model[fieldHeaderMapping[header as keyof typeof fieldHeaderMapping]] = idSetter(rowIndex);
+                } else {
+                    model[fieldHeaderMapping[header as keyof typeof fieldHeaderMapping]] = values[index];
+                }
             }
 
             return model;
