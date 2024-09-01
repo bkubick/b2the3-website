91ce0f397cd9e55e0fa76f14cf2b3b88f6621de0
bkubick <bjk2158@columbia.edu>
2024-09-01 09:31:20 -0500

    Add collapsible panel component

---
+++ web-app/src/components/panel/index.tsx
@@ -0,0 +1,9 @@
+/**
+ * @module components/pill - Pill Component
+ * @description a component that renders a pill with text inside.
+ */
+
+import CollapsiblePanel from './CollapsiblePanel';
+
+
+export { CollapsiblePanel };
